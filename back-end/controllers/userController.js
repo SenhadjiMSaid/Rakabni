b9f2c3d3ed391b345cbc905be09ea2e367f5d219
XeedOus <djezirixeedoussama@gmail.com>
2023-06-04 15:57:08 +0100

    main empty

--- back-end/controllers/userController.js
+++
@@ -1,8 +0,0 @@
-const User = require("./../models/userModel.js");
-const catchAsync = require("../utils/catchAsync");
-const factory = require("./handlerFactory");
-
-exports.getAllUsers = factory.getAll(User);
-exports.getUser = factory.getOne(User);
-exports.updateUser = factory.updateOne(User);
-exports.deleteUser = factory.deleteOne(User);
